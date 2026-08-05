87618d6faea18cd8fa8e8f21ea1317fd48518abd
Samit Bhisikar <samitbhisikar16@gmail.com>
2026-08-05 21:06:34 +0530

    feat:express,mongodb setup finalised

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './mongodb/connect.js'
 dotenv.config();
 
 const app=express()
@@ -12,7 +13,14 @@ app.get('/',async(req,res)=>{
 })
 
 const startServer=async()=>{
+
+try{
+    connectDB(process.env.MONGODB_URL)
     app.listen(8080,()=>console.log('Server has started on port http://localhost:8080'))
+}catch(error){
+    console.error('Error starting server:', error);
+}
+
 
 }
 startServer()
